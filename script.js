55c10461ae32e8eeba90fe1010c011f9649f2867
Ovine28 <53230378+Ovine28@users.noreply.github.com>
2025-08-11 23:10:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of a simple animation for the hero button
+document.querySelector('button').addEventListener('click', function() {
+    alert('Plus d\'informations bientôt disponibles!');
+});
